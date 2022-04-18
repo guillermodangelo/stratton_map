a3d2d131eff2d8a71bb029903080aadc1267bd0c
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2022-04-18 12:43:30 -0300

    CH

--- js/map.js
+++ js/map.js
@@ -92,27 +92,47 @@ function getColor(d) {
   }).addTo(map);
 
 
- const search = new GeoSearch.GeoSearchControl({
-  notFoundMessage: 'Address not found. Contact us to improve this tool.',
-  provider: new GeoSearch.OpenStreetMapProvider({
-    params: {
-      countrycodes: 'us',
-      viewbox: [-80.914, 25.115, -80.002, 26.379],
-      bounded: 1
-    }
-  }),
-  showMarker: false, // optional: true|false  - default true
-  showPopup: false, // optional: true|false  - default false
-  searchLabel: 'Find your STR regulation', // optional: string
-  //marker: {
-  //  icon: new L.Icon.Default(),
-  //  draggable: false,
-  //},
-  //popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
-  //resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
-  //keepResult: true, // optional: true|false  - default false
-  style: 'bar'
-});
+  // const search = new GeoSearch.GeoSearchControl({
+  //   notFoundMessage: 'Address not found. Contact us to improve this tool.',
+  //   provider: new GeoSearch.OpenStreetMapProvider({
+  //     params: {
+  //       countrycodes: 'us',
+  //       viewbox: [-80.914, 25.115, -80.002, 26.379],
+  //       bounded: 1
+  //     }
+  //   }),
+  //   showMarker: false, // optional: true|false  - default true
+  //   showPopup: false, // optional: true|false  - default false
+  //   searchLabel: 'Find your STR regulation', // optional: string
+  //   //marker: {
+  //   //  icon: new L.Icon.Default(),
+  //   //  draggable: false,
+  //   //},
+  //   //popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
+  //   //resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
+  //   //keepResult: true, // optional: true|false  - default false
+  //   style: 'bar'
+  // });
+
+
+  const search = new GeoSearch.GeoSearchControl({
+    notFoundMessage: 'Address not found. Contact us to improve this tool.',
+    provider: new GeoSearch.GoogleProvider({
+      params: {
+        key: '__API_KEY_HERE__',
+        language: 'en',
+        country: 'us',
+        region: 'us',
+        administrative_area_level_1: 'FL',
+        administrative_area_level_2: 'Miami-Dade, Broward'
+      }
+    }),
+    //retainZoomLevel: true,
+    showMarker: false, // optional: true|false  - default true
+    showPopup: false, // optional: true|false  - default false
+    searchLabel: 'Find your STR regulation', // optional: string
+    style: 'bar'
+  });
 
 
 map.addControl(search);
